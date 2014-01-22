807b4487c7b8b7dfc6eec7b7cf0c05437aa722da
Scott Shillcock <scott@shillcock.net>
2014-01-22 22:23:16 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BeardedArcher::Application.config.secret_key_base = 'badd9d1aa716ebce07137740e3dac321c45667444afabf511bd4e7094699706e4064ac6a32dbe2a7c6bd2b6bb9d5a8d28470728dd5c693af119b34393c3d96b5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BeardedArcher::Application.config.session_store :cookie_store, key: '_bearded-archer_session'
