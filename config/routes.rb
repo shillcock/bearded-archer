456aeae8116601dc2074c5397630fa87afe85ef4
Scott Shillcock <scott@shillcock.net>
2014-01-22 22:26:33 +0000

    scaffold workshop

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 BeardedArcher::Application.routes.draw do
+  resources :workshops
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
